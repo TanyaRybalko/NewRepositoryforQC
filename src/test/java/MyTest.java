e13077b27e7c9544d392dd3a1016b15ee85e7309
Tanya Rybalko <Rybalkota2@gmail.com>
2021-10-14 11:43:20 -0400

    first commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class MyTest {
+
+    @Test
+    public void testFirst() throws InterruptedException {
+        String driverPath = "C:/TanyaR/ChromeDriver/chromedriver.exe";
+        String chromeDriver = "webdriver.chrome.driver";
+        String CheckLine = "Check line";
+
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();   // open browser
+
+        driver.close();
+        driver.quit();
+    }
+
+
+
+}
